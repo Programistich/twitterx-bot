1fa684ed322c7bc45cadd5154107a58f9464f80c
Programistich <35292229+Programistich@users.noreply.github.com>
2026-01-07 17:49:21 +0200

    refactor: split bot init and start

--- internal/app/app.go
+++ internal/app/app.go
@@ -16,17 +16,17 @@ import (
 	"twitterx-bot/internal/twitterxapi"
 )
 
-func Run() error {
+func NewBot() (*gotgbot.Bot, *ext.Updater, *logger.Logger, error) {
 	cfg, err := config.Load()
 	if err != nil {
-		return err
+		return nil, nil, nil, err
 	}
 
 	l := logger.New(cfg.Debug)
 
 	bot, err := gotgbot.NewBot(cfg.BotToken, &gotgbot.BotOpts{})
 	if err != nil {
-		return fmt.Errorf("init bot: %w", err)
+		return nil, nil, nil, fmt.Errorf("init bot: %w", err)
 	}
 
 	dispatcher := ext.NewDispatcher(&ext.DispatcherOpts{
@@ -40,15 +40,37 @@ func Run() error {
 	apiClient := twitterxapi.NewClient(cfg.TwitterXAPIURL)
 	handlers.Register(dispatcher, l, apiClient)
 
+	return bot, updater, l, nil
+}
+
+func Start(bot *gotgbot.Bot, updater *ext.Updater, l *logger.Logger) error {
+	if bot == nil {
+		return fmt.Errorf("start bot: bot is nil")
+	}
+	if updater == nil {
+		return fmt.Errorf("start bot: updater is nil")
+	}
+
 	ctx, stop := signal.NotifyContext(context.Background(), os.Interrupt, syscall.SIGTERM)
 	defer stop()
 
 	if err := updater.StartPolling(bot, &ext.PollingOpts{DropPendingUpdates: true}); err != nil {
 		return fmt.Errorf("start polling: %w", err)
 	}
-	l.Info("bot started as @%s", bot.User.Username)
+	if l != nil {
+		l.Info("bot started as @%s", bot.User.Username)
+	}
 
 	<-ctx.Done()
 	updater.Stop()
 	return nil
+}
+
+func Run() error {
+	bot, updater, l, err := NewBot()
+	if err != nil {
+		return err
+	}
+
+	return Start(bot, updater, l)
 }
